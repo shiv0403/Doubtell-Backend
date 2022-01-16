cbefb5e9923d1e90ae8f538c228b4c3c12367f10
Shivansh Gupta <shiv0403gupta@gmail.com>
2022-01-16 16:28:38 +0530

    protecting routes middleware

---
+++ Middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookie.jwt;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+module.exports = { requireAuth };
