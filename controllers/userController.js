4e410e6fb165b5ff155d8f905c50581888bbd179
Shivansh Gupta <shiv0403gupta@gmail.com>
2022-01-29 16:37:51 +0530

    user answer info

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,4 +12,18 @@ const user_get = async (req, res) => {
   }
 };
 
-module.exports = { user_get };
+const user_answerInfo = async (req, res) => {
+  const { answerId, userId } = req.body;
+  try {
+    const user = await User.findOne({ _id: userId });
+    res.status(200).send({
+      user_likedPosts: user.user_likedPosts,
+      user_dislikedPosts: user.user_dislikedPosts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "unable to get info" });
+  }
+};
+
+module.exports = { user_get, user_answerInfo };
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
-const { user_get } = require("../controllers/userController");
+const { user_get, user_answerInfo } = require("../controllers/userController");
 const router = require("express").Router();
 
 router.get("/get-user/:id", user_get);
+router.post("/get-answerInfo", user_answerInfo);
 
 module.exports = router;
